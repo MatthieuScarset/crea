696c4cae00a4a9a525c50bda02d81bbbe13517a9
Matthieu SCARSET <m@matthieuscarset.com>
2021-11-17 14:39:28 +0100

    Restart client app from Truffle box React

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import UniversalPricing from "./contracts/UniversalPricing.json";
+import getWeb3 from "./getWeb3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+  state = { owner: null, web3: null, accounts: null, contract: null };
+
+  isLocalhost = Boolean(
+    window.location.hostname === "localhost" ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === "[::1]" ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
   );
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = UniversalPricing.networks[networkId];
+      const instance = new web3.eth.Contract(
+        UniversalPricing.abi,
+        deployedNetwork && deployedNetwork.address
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      );
+      console.error(error);
+    }
+  };
+
+  runExample = async () => {
+    const { accounts, contract } = this.state;
+
+    const response = await contract.methods.owner().call({ from: accounts[0] });
+
+    // Update state with the result.
+    this.setState({ owner: response });
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+        <header>
+          <img
+            className="App-logo"
+            src="logo.svg"
+            alt="Logo of the CREA project"
+          />
+          <h1 className="visually-hidden">CREA</h1>
+        </header>
+
+        <main>
+          <h2>Smart Contract Example</h2>
+          <div>The stored value is: {this.state.owner}</div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;

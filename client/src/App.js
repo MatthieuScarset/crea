39fa966d21ab8d11436c4d37cfce0df802c8176a
Matthieu SCARSET <m@matthieuscarset.com>
2021-11-20 22:41:24 +0100

    Fix responsive width

--- client/src/App.js
+++ client/src/App.js
@@ -160,7 +160,7 @@ class App extends Component {
     }
 
     return (
-      <div className="w-full md:w-1/3 m-auto flex flex-col space-y-10 md:space-y-8">
+      <div className="w-full px-4 md:w-1/2 lg:w-1/3 m-auto flex flex-col space-y-10 md:space-y-8">
         <header>
           <img
             className="m-auto pt-4 max-h-40"
